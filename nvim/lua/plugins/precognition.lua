fbdc5070219ec6c7a32d3104477d4484af23d2fb
Alex Ford <Alex@CatDadCode.dev>
2024-06-13 11:34:39 -0600

    feat(nvim): Adding toggle keys for Precognition plugin.

--- nvim/lua/plugins/precognition.lua
+++ nvim/lua/plugins/precognition.lua
@@ -1,6 +1,24 @@
+local enable = false
 return {
 	"tris203/precognition.nvim",
+	keys = {
+		{
+			"<leader>uP",
+			function()
+				enable = not enable
+				if enable then
+					require("precognition").show()
+					LazyVim.info("Precognition is enabled.")
+				else
+					require("precognition").hide()
+					LazyVim.warn("Precognition is disabled.")
+				end
+			end,
+			desc = "toggle Precognition",
+		},
+	},
 	opts = {
+		startVisible = true,
 		showBlankVirtLine = false,
 	},
 }
