e533b013dc161568c4d63dad1323eb76b4b76b29
Alex Ford <Alex@CatDadCode.dev>
2024-07-26 15:49:14 -0600

    fix(nvim): Remove markdown.nvim now that it is the default in the markdown extra package.

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,11 +1,5 @@
 return {
 	{
-		"MeanderingProgrammer/markdown.nvim",
-		event = "VeryLazy",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		opts = {},
-	},
-	{
 		"ellisonleao/glow.nvim",
 		config = true,
 		cmd = "Glow",
