e46036f1ec0fe5432d7f8da9f965570fa2b47a32
Chev <Chev@ChevCast.tv>
2024-09-19 15:13:07 -0600

    fix(neotest): Use opts function to avoid overriding adapters added by LazyVim extras.

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,15 +1,29 @@
 return {
-	{ "nvim-neotest/neotest-plenary" },
-	{ "marilari88/neotest-vitest" },
-	{ "thenbe/neotest-playwright" },
 	{
 		"nvim-neotest/neotest",
-		opts = {
-			adapters = {
-				"neotest-plenary",
-				"neotest-playwright",
-				"neotest-vitest",
-			},
+		lazy = false,
+		dependencies = {
+			-- Actual Dependencies
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+
+			-- Test Adapters
+			"nvim-neotest/neotest-plenary",
+			"marilari88/neotest-vitest",
+			"thenbe/neotest-playwright",
 		},
+		opts = function(_, opts)
+			opts.adapters["neotest-vitest"] = {}
+			opts.adapters["neotest-playwright"] = {
+				options = {
+					persist_project_selection = true,
+					enable_dynamic_test_discovery = true,
+				},
+			}
+			opts.adapters["neotest-plenary"] = {}
+			return opts
+		end,
 	},
 }
