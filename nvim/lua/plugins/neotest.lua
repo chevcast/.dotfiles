62bca029049f8e63df81012ca70f213b8f7a3396
Chev <Chev@ChevCast.tv>
2024-09-19 11:24:18 -0600

    fix: Following literal LazyVim example to see if that fixes adapter loading.

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,16 +1,14 @@
 return {
+	{ "nvim-neotest/neotest-plenary" },
+	{ "marilari88/neotest-vitest" },
+	{ "thenbe/neotest-playwright" },
 	{
 		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-neotest/neotest-plenary",
-			"marilari88/neotest-vitest",
-			"thenbe/neotest-playwright",
-		},
 		opts = {
 			adapters = {
 				"neotest-plenary",
-				"neotest-vitest",
 				"neotest-playwright",
+				"neotest-vitest",
 			},
 		},
 	},
